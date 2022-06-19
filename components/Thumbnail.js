4431c5b826d488a5ed907a2e988262469f10099e
abhitrivedi99 <abhitrivedi99@gmail.com>
2022-06-19 14:02:48 +0530

    fix(base-url) base-url changed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import { ThumbUpIcon } from '@heroicons/react/outline';
 import { forwardRef } from 'react';
 
 const Thumbnail = forwardRef(({ result }, ref) => {
-	const BASE_URL = process.env.BASE_URL;
+	const BASE_URL = 'https://image.tmdb.org/t/p/original';
 	return (
 		<div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
 			<Image
